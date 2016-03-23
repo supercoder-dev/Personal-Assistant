9abf8a8325a589d668b44d01669594473185dd29
Pavel Trutman <pavel.trutman@fel.cvut.cz>
2016-03-23 22:42:14 +0100

    Making dummy module for testing purpuses.

---
+++ modules/dummy/dummy.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+import zmq
+import time
+import sys
+
+class DummyModule:
+  """
+  Dummy module class, which just sends back what it receives.
+  """
+
+  def __init__(self, port):
+    """
+    Constructor of the dummy class.
+
+    Args:
+      port (int): port of the server
+
+    Returns:
+      None
+    """
+
+    self.port = port
+    self.zmqctx = zmq.Context()
+    self.runServer()
+
+
+  def runServer(self):
+    """
+    Runs the ZQM server.
+
+    Returns:
+      None
+    """
+
+    self.socket = self.zmqctx.socket(zmq.REP)
+    print('tcp://127.0.0.1:{}'.format(port))
+    self.socket.bind('tcp://127.0.0.1:{}'.format(port))
+
+
+  def listen(self):
+    """
+    Listens on the port.
+
+    Returns:
+      None
+    """
+
+    while True:
+      message = self.socket.recv().decode('utf-8')
+      print(message)
+      time.sleep(1)
+      self.reply('Reply: ' + message)
+
+
+  def reply(self, message):
+    """
+    Sends reply back to client.
+
+    Args:
+      message (str): message to send
+
+    Returns:
+      None
+    """
+
+    self.socket.send_string(str(message))
+
+
+if __name__ == '__main__':
+  port = sys.argv[1]
+  port = int(port)
+  dm = DummyModule(port)
+  dm.listen()
