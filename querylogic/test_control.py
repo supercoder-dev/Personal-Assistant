a1fbd4358ac20699047258aa899385450ff21447
Jakub Drápela <drapejak@fel.cvut.cz>
2016-03-24 11:51:55 +0100

    Deleting of unusable lines

--- querylogic/test_control.py
+++ querylogic/test_control.py
@@ -1,10 +1,7 @@
 # Python 3.4.3 
 
-# 21/Mar/2015
+# 24/Mar/2015
 # Drapela Jakub; drapejak@fel.cvut.cz 
-
-# for running this script install wit 
-# the website: https://wit.ai/docs/python/1.0.0/quickstart
 
 import festival
 from query_control import Query_control
@@ -21,30 +18,6 @@ for q in resp_array:
 	answer = qc.query_request(q)
 	print(answer)
 	festival.sayText(answer)
-
-
-
-#def wit_answer(my_query):
-#	resp = wit.text_query(my_query, 'CZFOG3SALT7JCMXSFJEI6PC3H4IRZESU')
-#	print('Wit response: {}'.format(resp), "\n")
-#	return resp
-
-
-#wit.init()
-#query=raw_input('Enter query: ');
-#print(query)
-#resp = wit_answer(query)
-#qc=Query_control();
-#answer=qc.query_request(resp);
-#print(answer);
-
-#wit.close()
-
-
-
-## unused
-
-
 
 
 
